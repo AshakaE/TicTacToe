cbc95537b39e6fde85002505289f503c2cb3fc62
KumarAmitt <amitt2015@outlook.com>
2021-01-13 14:13:56 +0530

    Create TODOs for the game

--- bin/main.rb
+++ bin/main.rb
@@ -20,6 +20,16 @@ s2 = left_symbol(s1)
 puts "#{name1} your symbol is #{s1}"
 puts "#{name2} your symbol is #{s2}"
 
+# 1. Create instances for player
 
+puts '-------Game Started-----------'
+
+# 2.Create an instance of Board Class
+
+# 3. Display the board
+
+# 4. Select our first player
+
+# 5. Loop until player wins or game draw (Ther is an interaction with Board class) 
 
 
