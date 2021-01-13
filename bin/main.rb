9fe19bd7a3f7c4ca385fe190cf5c62c168f4000c
KumarAmitt <amitt2015@outlook.com>
2021-01-13 19:46:17 +0530

    Implement validation functions for user name, Token and Position

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require_relative '../lib/board'
 require_relative '../lib/player'
 
@@ -15,18 +14,53 @@ def get_col(pos)
   %w[1 2 3].index(pos[1])
 end
 
+def player_name(num)
+  print "Player-#{num} Enter your name: "
+  name = gets.chomp
+  if name.empty?
+    puts 'Invalid Name'
+    player_name(num)
+  end
+  name.capitalize
+end
+
+def player_symbol(name)
+  print "#{name} Choose your LUCKY Token: "
+  sym = gets.chomp
+  if sym.empty? || sym.length > 1
+    puts 'Invalid Token. Please enter a single letter'
+    player_symbol(name)
+  end
+  sym.upcase
+end
+
+def valid_position?(pos)
+  pos.length == 2 && ('ABC'.include?(pos[0]) && '123'.include?(pos[1]))
+end
+
+def position(name)
+  print "#{name} Move: Enter Position[Example: A2]"
+  pos = gets.chomp.upcase
+  if valid_position?(pos)
+    pos
+  else
+    puts 'Invalid Position. Try different position'
+    position(name)
+  end
+end
+
 puts '-----Tic Tac Toe--------'
 
-puts 'Player-1 Enter your name:'
-name1 = gets.chomp.capitalize
-puts 'Player-2 Enter Your Name:'
-name2 = gets.chomp.capitalize
+name1 = player_name(1)
+name2 = player_name(2)
+name2 = name1 == name2 ? "#{name2}-1" : name2
 
 puts "Welcome #{name1} and #{name2}."
 
-puts "#{name1} please select your symbol [X or O]:"
-s1 = gets.chomp.upcase
-s2 = left_symbol(s1)
+s1 = player_symbol(name1)
+s2 = player_symbol(name2)
+s2 = s1 == s2 ? "#{s2}#{s2}" : s2
+
 puts "#{name1} your symbol is #{s1}"
 puts "#{name2} your symbol is #{s2}"
 
@@ -40,8 +74,7 @@ board.print_board
 active_player = p1
 
 loop do
-  puts "#{active_player.name} Move: Enter Position[Example: A2]"
-  pos = gets.chomp.upcase
+  pos = position(active_player.name)
   row = get_row(pos)
   col = get_col(pos)
 
@@ -53,7 +86,7 @@ loop do
   board.update_board(row, col, active_player.symbol)
   board.print_board
 
-  if board_full?
+  if board.board_full?
     puts '----Game Draw-----'
     break
   end
