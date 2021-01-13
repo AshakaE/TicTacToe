0e79b3fa66246cd603ae5521667ea259649d9aae
KumarAmitt <amitt2015@outlook.com>
2021-01-13 14:00:33 +0530

    Assign players to their name and symbol respectively

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+
+def left_symbol(s1)
+  s1 == 'X' ? 'O' : 'X'
+end
+
+puts '-----Tic Tac Toe--------'
+
+puts 'Player-1 Enter your name:'
+name1 = gets.chomp.capitalize
+puts 'Plater-2 Enter Your Name:'
+name2 = gets.chomp.capitalize
+
+puts "Welcome #{name1} and #{name2}."
+
+puts "#{name1} please select your symbol [X or O]:"
+s1 = gets.chomp.upcase
+s2 = left_symbol(s1)
+puts "#{name1} your symbol is #{s1}"
+puts "#{name2} your symbol is #{s2}"
+
+
+
+
